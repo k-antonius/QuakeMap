6fe77c852199dc7a04b1a55b611faf99fe4c678b
Kenneth LaMantia <kennethlamantia@gmail.com>
2017-10-14 10:26:51 -0400

    feature: Earthquake titles appear on feed update

--- js/app.js
+++ js/app.js
@@ -1,5 +1,4 @@
 'use strict';
-var map;
 
 class earthQuakeModel {
   constructor(geoJSON) {
@@ -13,15 +12,16 @@ class earthQuakeModel {
 }
 
 class MarkerManager {
-  constructor() {
+  constructor(googleMapInstance) {
     this.quakeMarker = null;
     this.placeMarker = null;
+    this.map = googleMapInstance;
   }
 
   createMarker(earthQuake) {
     return new google.maps.Marker({
       position: earthQuake.latLon,
-      map: map});
+      map: this.map});
   }
 
   addQuakeMarker(earthQuake) {
@@ -65,48 +65,27 @@ function ControlViewModel() {
   // use significant and week as default feed when app loads
   self.curFeedType = ko.observable("significant");
   self.curFeedTimeHorizon = ko.observable("week");
-  self.currentQuake = ko.observable(false);
-  self.markerManager = new MarkerManager();
 
-  // called from data binding to update current quake
+  self.currentQuake = ko.observable(false);
+  self.markerManager = null;
+
   self.setCurrentQuake = function(earthQuake) {
+    console.log(earthQuake);
     self.currentQuake(earthQuake);
   }
 
-  // marker appears on map when earthquake title is clicked
   self.currentQuake.subscribe(()=> {
     self.markerManager.addQuakeMarker(self.currentQuake());
   });
 
-  self.updateVisibleQuakes = function(bounds, loadedQuakes) {
+  function setVisibleQuakes (bounds, quakesToFilter) {
+    self.visibleQuakes(quakesToFilter.filter(quake => {
+      return bounds.contains(quake.latLon);
+    }));
+  }
 
-    function setVisibleQuakes (quakesToFilter) {
-      // self.visibleQuakes(quakesToFilter.filter(quake => {
-      //   return bounds.contains(quake.latLon);
-      // }));
-      console.log('in SetVisibleQuakes');
-      console.log(quakesToFilter);
-      self.visibleQuakes.removeAll();
-      for (let i=0; i < quakesToFilter.length; i++) {
-        console.log('in loop');
-        let curQuake = quakesToFilter[i];
-        if (bounds.contains(curQuake)) {
-          self.visibleQuakes.push(quakesToFilter[i]);
-        }
-        else {
-          console.log("not in bounds");
-        }
-      }
-    }
-
-    if (loadedQuakes) {
-      console.log('taking branch from arg');
-      console.log(loadedQuakes);
-      setVisibleQuakes(loadedQuakes);
-    }
-    else {
-      setVisibleQuakes(self.loadedQuakes);
-    }
+  self.updateVisibleQuakes = function(bounds) {
+    setVisibleQuakes(bounds, self.loadedQuakes);
   }
 
   // Generate a url for the desired earthquake feed
@@ -115,55 +94,46 @@ function ControlViewModel() {
    return baseFeedUrl;
   }
 
-
-
+  // load quakes from USGS and create new model objects
   async function getQuakeFeed() {
-    let loadedQuakes = [];
-    $.getJSON(self.generateFeedUrl(), function(data) {
-      for (var i = 0; i < data.features.length; i++) {
-       loadedQuakes.push(new earthQuakeModel(data.features[i]));
-      };
-    });
-    return loadedQuakes;
+    return $.getJSON(self.generateFeedUrl());
   }
 
-  /**
-  * @description After a UI update changes the USGS feed,
-  * this function loads the new geoJSON and updates the earthquakes
-  * listed in the UI.
-  */
+  self.populateQuakeModel = async function() {
+    let feedResults = await getQuakeFeed();
+    let newQuakes = [];
+    feedResults.features.forEach(feature => {
+      newQuakes.push(new earthQuakeModel(feature));
+    });
+    return newQuakes;
+  }
+
   self.updateQuakeFeed = async function() {
     if (self.map) {
-      let loadedQuakes = getQuakeFeed();
-      loadedQuakes.then(result => {
-        self.updateVisibleQuakes(self.map.getBounds(), result);
-        self.loadedQuakes = result;
-      });
+      let newQuakes = await self.populateQuakeModel();
+      self.loadedQuakes = newQuakes;
+      self.updateVisibleQuakes(self.map.getBounds());
     }
   }
-
-  // get earthquakes from feed
-
 
   // update the feed when either select menu changes
   self.curFeedType.subscribe(self.updateQuakeFeed, null);
   self.curFeedTimeHorizon.subscribe(self.updateQuakeFeed, null);
   // call for inital setup
   self.updateQuakeFeed();
-
 }
 
-// instatiate before passing to applyBindings so that
-// methods are available to initMap function
 var controlViewModel = new ControlViewModel();
 
 // create a new Google Map
 function initMap() {
-  map = new google.maps.Map(document.getElementById('map_container'), {
+  let map = new google.maps.Map(document.getElementById('map_container'), {
     center: {lat: 0, lng: 0},
     zoom: 3
   });
+
   controlViewModel.map = map;
+  controlViewModel.markerManager = new MarkerManager(map);
   // listener to let UI know that map bounds have changed
   map.addListener('idle', function() {
     let bounds = map.getBounds();
